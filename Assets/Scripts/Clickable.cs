5578019f880b12c0256a22504f61fc4a5f6493b0
Muhammad Umar <umar1okie@gmail.com>
2024-07-24 19:46:18 +0500

    Initial commit

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    [SerializeField] private Reticle reticleManager;
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+                if (hit.collider != null && hit.collider.gameObject == this.gameObject)
+                {
+                    reticleManager.Selected(this.gameObject);
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                reticleManager.Deselect();
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+            if (hit.collider != null && hit.collider.gameObject == this.gameObject)
+            {
+                reticleManager.Selected(this.gameObject);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            reticleManager.Deselect();
+        }
+    }
+}
